2e5419a0611ab69a6985ec1ba22603bff025eea5
unknown <lifei.ni@broadlink.com.cn>
2019-01-26 16:21:37 +0800

    init

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+/*
+    app.set()用于设置参数
+ */
+app.set('views', path.join(__dirname, 'views')); // 设置了模板文件夹的路径
+app.set('view engine', 'pug'); // 设置了使用的模板引擎
+
+/*
+    app.use()用于使用中间件
+ */
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public'))); // express.static()处理静态请求，该文件夹下的内容会以静态资料文件形式返回
+
+/*
+    配置路由
+ */
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
